a7ad88c30bfa2304e81ab89407b35c0658f13d78
tom mulvey <tommulvey5567@gmail.com>
2019-08-20 22:11:18 -0400

    starter console app with nuget pkgs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureFunctionExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
